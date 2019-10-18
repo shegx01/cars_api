8ff271b1b60871a3156bb1f7d92a67ae4e091231
O.O Samson <shegx01@gmail.com>
2019-10-18 23:23:58 +0100

    preparing db data

--- lib/cars_api/tools/generate_new_dataset.ex
+++ lib/cars_api/tools/generate_new_dataset.ex
@@ -1,31 +1,36 @@
-defmodule Generate.New.Dataset do
-  alias CarsApi.Tools.PopulateDb
-
-  def transform_elixir_map_struct(list) do
-    list
-   |> Enum.reduce(%{}, &transform_data/2)
+defmodule CarsApi.Tools.GenerateNewDataset do
+  def transform_elixir_map_struct(bin_data) do
+    bin_data
+    |> decode_bin_to_map
+    |> Enum.map(&transform_data/1)
   end
 
-  defp transform_data({key, value}, map) do
-    Map.put(map, convert_key(key), value)
-
+  defp transform_data(map) do
+    for {key, value} <- map, into: %{} do
+      {convert_to_pascal(key), value}
+    end
   end
 
-  defp convert_key(key) do
-    key
-    |> PopulateDb.to_pascal_case()
-    |>String.to_atom()
-  end
-  def decode_bin(bin_file_path) do
-    bin_file_path
-    |> decode_bin_file
-  end
-  defp decode_bin_file(path) do
+  defp decode_bin_to_map(path) do
     with {:ok, binary} <- File.read(path),
          {:ok, file} <- :erlang.binary_to_term(binary) do
       file
       |> String.replace("\n", "")
       |> Jason.decode()
+    end
+  end
+
+  defp convert_to_pascal(word) do
+    pattern = ~r/[A-Z]/
+
+    case Regex.scan(pattern, word) do
+      [a] ->
+        str = "_" <> (a |> to_string() |> String.downcase())
+        [first, last] = String.split(word, pattern)
+        first <> str <> last
+
+      _ ->
+        word
     end
   end
 end
