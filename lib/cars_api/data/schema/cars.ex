19ae8bc81cbbb77049a948bdcc5466dd48a8298f
O.O Samson <shegx01@gmail.com>
2019-10-19 11:31:04 +0100

    re_indexed db && shaped schema

--- lib/cars_api/data/schema/cars.ex
+++ lib/cars_api/data/schema/cars.ex
@@ -4,8 +4,21 @@ defmodule CarsApi.Data.Schema.Cars do
 
   @type t :: %__MODULE__{}
 
-  @permitted_fields [:stock_number, :year, :make, :model, :body, :trim, :base_price, :msrp, :image_count, :mileage, :interior_color, :transmission, :features, :images]
-
+  @permitted_fields [
+    :stock_number,
+    :year,
+    :make,
+    :model,
+    :body,
+    :trim,
+    :base_price,
+    :msrp,
+    :image_count,
+    :mileage,
+    :interior_color,
+    :transmission,
+    :features,
+  ]
 
   schema "cars" do
     field :stock_number, :integer
@@ -14,25 +27,19 @@ defmodule CarsApi.Data.Schema.Cars do
     field :model, :string
     field :body, :string
     field :trim, :string
-    field :base_price,  Money.Currency.Ecto.Type
-    field :msrp, :integer
+    field :base_price,  Money.Ecto.Currency.Type
+    field :msrp, :float
     field :image_count, :integer
     field :mileage, :integer
     field :exterior_color, :string
     field :interior_color, :string
     field :transmission, :string
     field :features, {:array, :string}
-    field :images, {:array, :string}
 
     timestamps()
   end
 
-
-
-
-
-
-  def changeset(%__MODULE__{} = model, params\\%{}) do
+  def changeset(%__MODULE__{} = model, params \\ %{}) do
     model
     |> cast(params, @permitted_fields)
   end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,17 @@
-#db seed
-alias CarsApi.Repo
-alias CarsApi.Data.Schema.Cars
-import Ecto.Changeset only: [change: 2]
+# db seed
+# alias CarsApi.Repo
+# alias CarsApi.Data.Schema.Cars
+# import Ecto.Changeset only: [change: 2]
 
-lists = CarsApi.Tools.GenerateNewDataset.transform_elixir_map_struct("/home/shegx01/Desktop/cars_api/cars_api/items.bin")
+lists =
+  CarsApi.Tools.GenerateNewDataset.transform_elixir_map_struct(
+    "/home/shegx01/Desktop/cars_api/cars_api/items.bin"
+  )
 
-Enum.each(list, fn ->
-
-  Repo.insert!(struct, opts \\ [])
+Enum.each(lists, fn car ->
+  with price <- Map.get(car, "base_price", 0),
+       new_map <- %{car | "base_price" => Money.new(round(price))},
+       changeset <- CarsApi.Data.Schema.Cars.changeset(%CarsApi.Data.Schema.Cars{}, new_map) do
+    CarsApi.Repo.insert!(changeset)
+  end
 end)
