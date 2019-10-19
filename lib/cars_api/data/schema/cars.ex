97e2e59326a009e0a69ee9785cdffc588fd8a6ca
O.O Samson <shegx01@gmail.com>
2019-10-19 15:05:37 +0100

    fixing contraint issue

--- lib/cars_api/data/schema/cars.ex
+++ lib/cars_api/data/schema/cars.ex
@@ -18,23 +18,25 @@ defmodule CarsApi.Data.Schema.Cars do
     :interior_color,
     :transmission,
     :features,
+    :image
   ]
 
   schema "cars" do
-    field :stock_number, :integer
+    field :stock_number, :integer, null: false
     field :year, :integer
     field :make, :string
     field :model, :string
     field :body, :string
     field :trim, :string
-    field :base_price,  Money.Ecto.Currency.Type
+    field :base_price, Money.Ecto.Map.Type
     field :msrp, :float
-    field :image_count, :integer
+    field :image_count, :integer, null: false, default: 0
     field :mileage, :integer
     field :exterior_color, :string
     field :interior_color, :string
     field :transmission, :string
     field :features, {:array, :string}
+    field :image, :string
 
     timestamps()
   end
@@ -42,5 +44,6 @@ defmodule CarsApi.Data.Schema.Cars do
   def changeset(%__MODULE__{} = model, params \\ %{}) do
     model
     |> cast(params, @permitted_fields)
+    |> unique_constraint(:stock_number)
   end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,17 +1,44 @@
 # db seed
-# alias CarsApi.Repo
-# alias CarsApi.Data.Schema.Cars
-# import Ecto.Changeset only: [change: 2]
+alias CarsApi.Data.Schema.Cars
 
 lists =
-  CarsApi.Tools.GenerateNewDataset.transform_elixir_map_struct(
-    "/home/shegx01/Desktop/cars_api/cars_api/items.bin"
-  )
+  
+
+remove_nil = fn map_item, item_key, value ->
+  case Map.get(map_item, item_key) do
+    nil ->
+      Map.put(map_item, item_key, value)
+
+    _ ->
+      map_item
+  end
+end
 
 Enum.each(lists, fn car ->
-  with price <- Map.get(car, "base_price", 0),
-       new_map <- %{car | "base_price" => Money.new(round(price))},
-       changeset <- CarsApi.Data.Schema.Cars.changeset(%CarsApi.Data.Schema.Cars{}, new_map) do
-    CarsApi.Repo.insert!(changeset)
+  with car_not_nil <- remove_nil.(car, "image_count", 0),
+       price <- Map.get(car_not_nil, "base_price", 0),
+       car_with_price <- %{car | "base_price" => Money.new(round(price))} do
+    car_with_img =
+      case Map.get(car_with_price, "image_count") do
+        nil ->
+          Map.put(
+            car_with_price,
+            "image",
+            "https://img2.carmax.com/content/images/ComingSoon.jpg?width=1200&quality=80&mode=crop&height=300"
+          )
+
+        _ ->
+          %{"stock_number" => stock_number} = car_with_price
+
+          Map.put(
+            car_with_price,
+            "image",
+            "https://img2.carmax.com/img/vehicles/#{stock_number}/1.jpg?&width=1200"
+          )
+      end
+
+    Cars.changeset(%Cars{}, car_with_img) |> CarsApi.Repo.insert!()
   end
+
+  # replace nil value with 0
 end)
