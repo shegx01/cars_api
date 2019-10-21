1c8e055e9ed103503836b386f63eb83d66d42b49
O.O Samson <shegx01@gmail.com>
2019-10-21 09:34:52 +0100

    car context models

--- lib/cars_api/data/model/car_context.ex
+++ lib/cars_api/data/model/car_context.ex
@@ -1,4 +1,20 @@
 defmodule CarsApi.Data.Model.CarContext do
-# to be implemented
+  alias CarsApi.Repo
+  import Ecto.Query
+  alias CarsApi.Data.Schema.Cars
 
+
+
+  def all() do
+    Repo.all(Cars)
+  end
+
+  def get_by_stock_num(stock_num) do
+    query =
+      from(c in Cars, where: c.stock_number == ^stock_num )
+
+      Repo.one query
+  end
+
+  
 end
